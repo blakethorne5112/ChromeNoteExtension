cc8ae7be12f1d7858eb860e437c389ef63192be2
Dylan208 <dylanperrin2002@gmail.com>
2024-09-03 20:17:38 +1000

    API for AI detection (non-functional)

--- background.js
+++ background.js
@@ -30,3 +30,56 @@ function scrapePageContent(callback) {
 }
 
   
+// Implementation of AI text detection API (Sapling.ai) TO DO
+//The following does not work yet, needs to be worked on further
+/*
+import './App.css';
+
+import { useEffect } from 'react';
+import { Sapling } from "@saplingai/sapling-js/observer";
+
+function App() {
+  useEffect(() => {
+    Sapling.init({
+      endpointHostname: 'http://127.0.0.1:5000',
+      saplingPathPrefix: '/sapling',
+    });
+
+    const editor = document.getElementById('editor');
+    Sapling.observe(editor);
+
+    const button = document.getElementById('ai-detect-btn');
+    button.addEventListener('click', () => {
+      Sapling.checkOnce(editor, {aiDetect: true});
+    });
+  });
+
+  return (
+    <>
+      <div
+        id="editor"
+        sapling-ignore="true"
+        contentEditable="true"
+        style={{
+          margin: '40px auto',
+          padding: '10px',
+          border: '2px solid black',
+          width: '500px',
+        }}>
+        Phone chargers are essential accessories that have become an integral part of modern life. These devices are designed to replenish the battery life of our smartphones and other electronic gadgets, enabling us to stay connected and productive throughout the day. With advancements in technology, phone chargers have evolved significantly, offering various types such as wired, wireless, fast-charging, and portable chargers.
+        <br/><br/>
+        The convenience and versatility of phone chargers have revolutionized the way we use our devices, ensuring we can always stay powered up and connected in our fast-paced, digital world. Portable phone chargers, also known as power banks, have become a lifesaver for people on the go. These compact, battery-powered devices store electrical energy that can be used to charge our phones when an outlet is unavailable. Whether traveling, camping, or during emergencies, power banks ensure we are never left stranded with a dead phone battery.
+      </div>
+      <button id="ai-detect-btn" style={{
+          padding: '10px',
+          border: '2px solid black',
+          width: '500px',
+          height: '40px',
+          cursor: 'pointer',
+        }}>Check AI</button>
+    </>
+  );
+}
+
+export default App;
+*/
